c465e85295ded4bf93dfdfb760d9d57be6632b07
maalid <maalid@gmail.com>
2020-09-27 18:28:48 -0300

    forst commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -15,12 +15,12 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "seRialConnectoR", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional) 
+  pkg_title = "Connect to serial port", # The Title of the package containing the App 
+  pkg_description = "A shiny app to send a signal to an Arduino using serial port.", # The Description of the package containing the App 
+  author_first_name = "Marcelo", # Your First Name
+  author_last_name = "Alid", # Your Last Name
+  author_email = "maalid@gmail.com", # Your Email
+  repo_url = "https://github.com/maalid/seRialConnectoR" # The URL of the GitHub Repo (optional) 
 )     
 
 ## Set {golem} options ----
